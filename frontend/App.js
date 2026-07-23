44743693d19ba339be436067bd4e5ca18f0db165
miguel-a-s-santos <miguelangelo.ss.pessoal@gmail.com>
2026-07-22 21:51:30 -0300

    chore(frontend): configura EAS build e dependências base

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/services/auth';
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <AppNavigator />
       <StatusBar style="auto" />
-    </>
+    </AuthProvider>
   );
 }
